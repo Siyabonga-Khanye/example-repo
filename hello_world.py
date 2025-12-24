fa2279f09630fb31ceca05768872689dfcb5892a
Siyabonga-Khanye <siyabongakhanye10@gmail.com>
2025-12-24 13:11:42 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+""""
+request username
+store input in variable called "name"
+    print "name"
+    request user age
+store input in variable called "age"
+    print "age"
+If input equals to "age"
+     print "Hello, World! "
+
+"""
+
+name = input ("Enter your name: ")
+
+print ("name")
+
+age = input ("Enter your age: ")
+
+print ("age")
+
+print ("Hello, World! ")
+
+
+
+
